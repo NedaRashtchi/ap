b444036e0e8bbbd06cd379739e10f6b35b582043
NedaRashtchi <ndarshtchi@gmail.com>
2025-02-28 01:25:04 +0330

    first

---
+++ src/E5_2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class E5_2 {
+    public static void main(String[] args) {
+        System.out.print("Enter any number: ");
+        Scanner in = new Scanner(System.in);
+        double number = in.nextDouble();
+        if(number == 0) {
+            System.out.println("The number is zero");
+        }else if(number > 0) {
+            System.out.println("The number is positive");
+        }else if(number < 0) {
+            System.out.println("The number is negative");
+        }
+        if(number < 1) {
+            System.out.println("Its a small number");
+        }
+        if(number > 1000000) {
+            System.out.println("Its a large number");
+        }
+
+    }
+}
