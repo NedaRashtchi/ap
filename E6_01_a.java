c497347c838aadf9c91dcb6ad11f59887e5d2e2f
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-03 23:41:27 +0330

    E6.1.a

---
+++ E6_01_a.java
@@ -0,0 +1,9 @@
+public class E6_01_a {
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=2;i<=100;i+=2){
+            sum+=i;
+        }
+        System.out.println("the sum of even numbers between 2 and 100(inclusive) : "+sum);
+    }
+}
