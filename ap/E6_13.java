7b541474e25d97456ee60249cc56321b6a6334d0
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-06 14:54:16 +0330

    E6.13

---
+++ ap/E6_13.java
@@ -0,0 +1,18 @@
+package ap;
+
+import java.util.Scanner;
+
+public class E6_13 {
+    public static void main(String[] args) {
+        System.out.print("Enter a number : ");
+        Scanner number = new Scanner(System.in);
+        int num = number.nextInt();
+        printBinary(num);
+    }
+    static void printBinary(int num){
+        while(num>0){
+            System.out.println(num%2);
+            num/=2;
+        }
+    }
+}
