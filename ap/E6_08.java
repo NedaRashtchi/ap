b177917bf5dfc48297e33f878d918125557e0aab
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-06 14:23:36 +0330

    E6.8

---
+++ ap/E6_08.java
@@ -0,0 +1,15 @@
+package ap;
+
+import java.util.Scanner;
+
+public class E6_08 {
+    public static void main(String[] args) {
+        System.out.println("input string : ");
+        String str ;
+        Scanner s = new Scanner(System.in);
+        str = s.nextLine();
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+}
