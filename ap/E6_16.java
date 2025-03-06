00273d8035018a89d8921b36a3e89d099a711218
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-06 15:01:41 +0330

    E6.16

---
+++ ap/E6_16.java
@@ -0,0 +1,12 @@
+package ap;
+
+public class E6_16 {
+    public static void main(String[] args) {
+        for(int i=1; i<=10 ; i++){
+            for (int j=1 ;j<=10 ;j++){
+                System.out.format("%4d" , i*j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
