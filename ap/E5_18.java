31ded9ce4b387deb2fac1d16a920fa55e07712c4
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-07 23:09:46 +0330

    E5.18

--- ap/E5_18.java
+++ ap/E5_18.java
@@ -10,14 +10,17 @@ public class E5_18 {
         for (int i = 0; i < name.length; i++) {
             name[i] = s.nextLine();
         }
-        /*String temp;
-        for(int i=0;i<name.length-1;i++){
-            if(name[i].compareTo(name[i+1])>0){
-                temp = name[i];
-                name[i] = name[i+1];
-                name[i+1] = temp;
+ /*       String temp;
+        for(int j=0;j<name.length;j++){
+            for(int i=0;i<name.length-1;i++){
+                if(name[i].compareTo(name[i+1])>0){
+                    temp = name[i];
+                    name[i] = name[i+1];
+                    name[i+1] = temp;
+                }
             }
         }*/
+
         Arrays.sort(name);
         for(int i=0;i< name.length ;i++){
             System.out.println(name[i]);
