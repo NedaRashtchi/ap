232f2976611c69191e43f5fd18c1610dee7e0254
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-07 15:45:44 +0330

    E5.18

---
+++ ap/E5_18.java
@@ -0,0 +1,27 @@
+package ap;
+
+import java.util.*;
+
+public class E5_18 {
+    public static void main(String[] args) {
+        System.out.println("Enter 3 strings: ");
+        Scanner s = new Scanner(System.in);
+        String[] name = new String[3];
+        for (int i = 0; i < name.length; i++) {
+            name[i] = s.nextLine();
+        }
+        /*String temp;
+        for(int i=0;i<name.length-1;i++){
+            if(name[i].compareTo(name[i+1])>0){
+                temp = name[i];
+                name[i] = name[i+1];
+                name[i+1] = temp;
+            }
+        }*/
+        Arrays.sort(name);
+        for(int i=0;i< name.length ;i++){
+            System.out.println(name[i]);
+        }
+    }
+}
+
