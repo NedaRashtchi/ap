10046cb356d0072195dfbc8806ea03446e277443
NedaRashtchi <ndarshtchi@gmail.com>
2025-03-24 15:42:28 +0330

    EX2_PM_1_2

---
+++ ap/exercises/ex2/EX2_PM_1_2.java
@@ -0,0 +1,23 @@
+package ap.exercises.ex2;
+
+import java.util.Scanner;
+
+public class EX2_PM_1_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        char [][]board = new char[k+2][k+2];
+        for(int i = 0; i < k+2; i++){
+            for(int j = 0; j < k+2; j++){
+                if(i==0 || j==0 || j==k+1 || i==k+1){
+                    board[i][j] = '*';
+                }
+                else board[i][j] = ' ';
+            }
+        }
+        for(char []b : board){
+            System.out.print(b);
+            System.out.println();
+        }
+    }
+}
