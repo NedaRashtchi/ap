10f82672090223b3b15f52a08a0ef7bf42fc86a0
NedaRashtchi <ndarshtchi@gmail.com>
2025-04-29 01:11:23 +0330

    E3.12

---
+++ ap/exercises/ex4/E3_12.java
@@ -0,0 +1,21 @@
+package ap.exercises.ex4;
+
+public class E3_12 {
+    private String employeeName;
+    private double currentSalary;
+
+    public E3_12(String employeeName, double currentSalary){
+        this.employeeName = employeeName;
+        this.currentSalary = currentSalary;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+    public double getCurrentSalary() {
+        return currentSalary;
+    }
+    public void raiseSalary(double byPercent){
+        currentSalary += (currentSalary*byPercent*0.01);
+    }
+}
---
+++ ap/exercises/ex4/EmployeeTester.java
@@ -0,0 +1,12 @@
+package ap.exercises.ex4;
+
+public class EmployeeTester {
+    public static void main(String[] args) {
+        E3_12 harry = new E3_12("harry",53400);
+
+        System.out.println("Name: " + harry.getEmployeeName());
+        System.out.println("Salary before raise: " + harry.getCurrentSalary());
+        harry.raiseSalary(10);
+        System.out.println("Salary after raise: " + harry.getCurrentSalary());
+    }
+}
