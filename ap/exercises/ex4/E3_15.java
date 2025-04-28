b8a2208b4474ebc5e728450283828f14c19bfb6c
NedaRashtchi <ndarshtchi@gmail.com>
2025-04-29 01:37:08 +0330

    E3.15

---
+++ ap/exercises/ex4/E3_15.java
@@ -0,0 +1,20 @@
+package ap.exercises.ex4;
+
+public class E3_15 {
+    private String from;
+    private String to;
+    private String body = "";
+
+    public E3_15(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public void addLine(String line) {
+        body = body.concat(line + "\n");
+    }
+
+    public String getText() {
+        return "Dear " + to + ":\n\n" + body + "\nSincerely,\n" + from;
+    }
+}
---
+++ ap/exercises/ex4/LetterPrinter.java
@@ -0,0 +1,15 @@
+package ap.exercises.ex4;
+
+public class LetterPrinter {
+    public static void printLetter(E3_15 letter) {
+        System.out.println(letter.getText());
+    }
+
+    public static void main(String[] args) {
+        E3_15 letter = new E3_15("Mary", "John");
+        letter.addLine("I am sorry we must part.");
+        letter.addLine("I wish you all the best.");
+
+        LetterPrinter.printLetter(letter);
+    }
+}
