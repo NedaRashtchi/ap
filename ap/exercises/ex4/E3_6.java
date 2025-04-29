9e4506a8b097a6dca4588b89c808a2cbdb9998e7
NedaRashtchi <ndarshtchi@gmail.com>
2025-04-29 21:55:44 +0330

    EX4_E3_6

--- ap/exercises/ex4/E3_6.java
+++ ap/exercises/ex4/E3_6.java
@@ -11,53 +11,56 @@ public class E3_6 {
         this.hallwayLight = getLampState();
     }
 
-    public int getSwitchState(int switchNum){
-        if(switchNum == 1){ return firstSwitch; }
-        else if(switchNum == 2){ return secondSwitch; }
-        else {
+    public int getSwitchState(int switchNum) {
+        if (switchNum == 1) {
+            return firstSwitch;
+        } else if (switchNum == 2) {
+            return secondSwitch;
+        } else {
             System.out.println("error");
             System.exit(0);
         }
         return -1;
     }
 
-    public int getLampState(){ // 0 for off, 1 for on
+    public int getLampState() { // 0 for off, 1 for on
 
-        if(firstSwitch == secondSwitch){
+        if (firstSwitch == secondSwitch) {
             hallwayLight = 0;
-        }else hallwayLight = 1;
+        } else hallwayLight = 1;
 
         return hallwayLight;
     }
-    public void toggleSwitch(int switchNum){
-        if(switchNum == 1){
-            if(firstSwitch == 1) firstSwitch = 0;
+
+    public void toggleSwitch(int switchNum) {
+        if (switchNum == 1) {
+            if (firstSwitch == 1) firstSwitch = 0;
             else firstSwitch = 1;
-        }
-        else if(switchNum == 2){
-            if(secondSwitch == 1) secondSwitch = 0;
+        } else if (switchNum == 2) {
+            if (secondSwitch == 1) secondSwitch = 0;
             else secondSwitch = 1;
-        }
-        else {
+        } else {
             System.out.println("error");
         }
     }
 
-    public String getSwitchesState(int state){
-        if(state == 1) return "Up";
+    public String getSwitchesState(int state) {
+        if (state == 1) return "Up";
         return "Down";
     }
-    public String getLightState(int state){
-        if(state == 1) return "On";
+
+    public String getLightState(int state) {
+        if (state == 1) return "On";
         return "Off";
     }
-    public void printState(E3_6 hallwayLight){
+
+    public void printState(E3_6 hallwayLight) {
         System.out.println("Hallway Light : " + hallwayLight.getLightState((hallwayLight.getLampState())));
         System.out.println("First Switch : " + hallwayLight.getSwitchesState(hallwayLight.getSwitchState(1)));
         System.out.println("Second Switch : " + hallwayLight.getSwitchesState(hallwayLight.getSwitchState(2)));
     }
 
-    public static void main(String[] args) {
+    public static void main() {
         E3_6 hallwayLight = new E3_6(1, 1);
 
         hallwayLight.printState(hallwayLight);
