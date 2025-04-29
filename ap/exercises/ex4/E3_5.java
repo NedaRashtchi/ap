83ea6cb953026d1321d19c4fd64ca29e7814cb2c
NedaRashtchi <ndarshtchi@gmail.com>
2025-04-29 21:54:10 +0330

    EX4_E3_5

--- ap/exercises/ex4/E3_5.java
+++ ap/exercises/ex4/E3_5.java
@@ -7,16 +7,8 @@ public class E3_5 {
     public E3_5(int firstSwitch, int secondSwitch) {
         hallwayLight = new E3_4(firstSwitch, secondSwitch);
     }
-//    private boolean check(){
-//        if(hallwayLight.getFirstSwitchState() == hallwayLight.getSecondSwitchState()){
-//            if(hallwayLight.getLampState() == 0) return true;
-//            else return false;
-//        }else {
-//            if(hallwayLight.getLampState() == 0) return false;
-//            else return true;
-//        }
-//    }
-    private void test(){
+
+    private void test() {
         System.out.println("Current State : ");
         hallwayLight.printState(hallwayLight);
         System.out.println("-------------------");
@@ -36,7 +28,7 @@ public class E3_5 {
 
     }
 
-    public static void main(String[] args) {
+    public static void main() {
         E3_5 circuitTester = new E3_5(1, 0);
         circuitTester.test();
     }
